30b35c617505b26c95e92b83654fe68698feb8fb
Preston Davis <prem.dav@outlook.com>
2019-10-12 17:15:44 -0500

    Added style loaders and react router to project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App/App';
+import './Stylesheets/main.sass';
 
 const title = 'React with webpack and babel';
 
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,15 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
-      }
+      },
+      {
+        test:/\.css$/,
+        use:['style-loader', 'css-loader']
+      },
+      {
+        test:/\.sass$/,
+        use:['style-loader','css-loader', 'sass-loader']
+     }
     ]
   },
   resolve: {
