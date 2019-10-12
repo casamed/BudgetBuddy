8b0a190cfff45b6ecbf1f9407120334d58d08f2d
Preston Davis <prem.dav@outlook.com>
2019-10-12 17:38:45 -0500

    Cleanup

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import App from './App/App';
 import './Stylesheets/main.sass';
 
-const title = 'React with webpackbabel';
-
 ReactDOM.render(
-  <App title={title} />,
+  <App />,
   document.getElementById('app')
 );
 
