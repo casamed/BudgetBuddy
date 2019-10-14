9121333f270e8d38b860d7f8550e665a37b5b916
Casey Medford <case_med@hotmail.com>
2019-10-14 11:02:18 -0500

    Working on Background Image

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
-import MainLanding from '../MainLanding/Main'
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
     <div className="fullScreen landingBack">
       <NavBar/>
-      <MainLanding/>
+      <div className="container">
+            <h1 className="cover-heading">Welcome</h1>
+            <p className="lead">Budget Buddy is a Web App that allows you to create and track your expenses!</p>
+            <p className="lead">
+                <Link to='/Login' className="btn btn-lg btn-secondary">Login</Link>
+            </p>
+        </div>
     </div>
   );
 };
