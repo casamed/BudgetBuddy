f66961443fdb8a0486d7c04f9afcdf264de9ee72
Casey Medford <case_med@hotmail.com>
2019-10-16 10:26:06 -0500

    Changes requested for pull

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -11,7 +11,7 @@ const Landing = () => {
                 <h1 className="cover-heading">Welcome</h1>
                 <p className="lead">Budget Buddy is a Web App that allows you to create and track your expenses!</p>
                 <p className="lead">
-                    <Link to='/Login' className="btn btn-lg btn-secondary">Login</Link>
+                    <Link to='/login' className="btn btn-lg btn-secondary">Login</Link>
                 </p>
             </div>
         </div>
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -4,12 +4,11 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
         <nav className="navbar navbar-expand-lg navbar-light navBack ">
-            <a className="navbar-brand" href="#">Budget Buddy</a>
-        
+            <h2 className="nav-item">Budget Buddy</h2>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ml-auto ">
                     <li className="nav-item">
-                    <Link to='/Login' className="nav-link">Login</Link>
+                    <Link to='/login' className="nav-link">Login</Link>
                     </li>
                 </ul>
             </div>
